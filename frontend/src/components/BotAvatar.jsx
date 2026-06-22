68ed00bc6660fe57efc926fa5e949f3cc6272b63
thiagoventurav <thiago2013ventura@gmail.com>
2026-06-22 20:35:56 -0300

    remover front do repoback

--- frontend/src/components/BotAvatar.jsx
+++
@@ -1,7 +0,0 @@
-export default function BotAvatar() {
-  return (
-    <div className="chat-header-logo" style={{ marginTop: '2px' }}>
-      <i className="fa-solid fa-star text-white" style={{ fontSize: '11px' }} />
-    </div>
-  )
-}
